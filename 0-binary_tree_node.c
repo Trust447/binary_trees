eabbd438b3e674fcc1f7ff157a5148111ac298b1
trust <isaactrust447@gmail.com>
2023-11-30 19:44:42 +0100

    function that creates a node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+/**
+ * binary_tree_t *binary_tree_node - fuction that creates a binary tree node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ * Return: return a pointer to the new node if sucessful or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL || value == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (parent == NULL && value != NULL)
+		{
+			new_node = malloc(sizeof(binary_tree_t));
+		}
+	}
+	return (new_node);
+
+}
